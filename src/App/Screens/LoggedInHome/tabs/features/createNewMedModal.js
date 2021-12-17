2c4d76442a91ad93b83cbc76617deb07858d1cc4
kbittner15 <61249059+kbittner15@users.noreply.github.com>
2021-12-16 23:56:08 -0500

    added meds

--- src/App/Screens/LoggedInHome/tabs/features/createNewMedModal.js
+++ src/App/Screens/LoggedInHome/tabs/features/createNewMedModal.js
@@ -4,16 +4,18 @@ import { View, Text,  TextInput, TouchableOpacity ,StyleSheet} from "react-nativ
 
 import styles from '../Styles/styles'
 
-const loginScreen = ({
-  logUserIn,
-  setEmail,
-  setPassword,
-  email,
-  password,
+const CreateNewMedModal = ({
+    setNewMedName,
+    setNewMedTimes,
+    setNewMedFrequency,
+    atTimesToTake,
+    name,
+    takeFrequency,
+    createNewMed,
 }) => {
   
   const validate = () => {
-    logUserIn()
+    createNewMed()
   }
 
 
@@ -21,19 +23,26 @@ const loginScreen = ({
   return(     
       <View style = {{paddingTop:200}}>
       <Text></Text>
-      <Text>Email</Text>
+      <Text>Medication Name </Text>
           <TextInput 
               style = {styles.inputStyle}
-              placeholder = "email"
-              onChangeText = {(email) => setEmail(email)}
-              value = {email}
+              placeholder = "name"
+              onChangeText = {(name) => setNewMedName(name)}
+              value = {name}
               />
-          <Text>Password</Text>
+          <Text>Medication frequency</Text>
+          <TextInput 
+              style = {styles.inputStyle}
+              placeholder = "frequency"
+              onChangeText = {(takeFrequency) => setNewMedFrequency(takeFrequency)}
+              value = {takeFrequency}
+              />
+        <Text>Medication time</Text>
           <TextInput 
               style = {styles.inputStyle}
               placeholder = "Password"
-              onChangeText = {(password) => setPassword(password)}
-              value = {password}
+              onChangeText = {(atTimesToTake) => setNewMedTimes(atTimesToTake)}
+              value = {atTimesToTake}
               />
           <TouchableOpacity 
           onPress={() => {
@@ -53,12 +62,17 @@ const loginScreen = ({
 
 export default connect(
     ({medReducer}) => ({
-     date: medReducer.newMed.date,
+     atTimesToTake: medReducer.newMed.atTimesToTake,
+     name: medReducer.newMed.name,
+     takeFrequency: medReducer.newMed.takeFrequency
     }),
 
      (dispatch, ownProps) => ({
-      createNewMed: (email) => dispatch(actions.med.createNewMed(email)),
+      setNewMedName: (email) => dispatch(actions.med.SetNewMedName(email)),
+      setNewMedTimes: (times) => dispatch(actions.med.SetNewMedTimes(times)),
+      setNewMedFrequency: (frequency) => dispatch(actions.med.SetNewMedFrequency(frequency)),
+      createNewMed: () => dispatch(actions.med.CreateNewMed()),
       ...ownProps
     }),
 
-  )(loginScreen)
+  )(CreateNewMedModal)
